77e5c3aed199c1c92a9b59b30ceaf06c7f5e49fb
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-09-18 11:44:51 -0400

    dropdown-sign

--- coffee_news.js
+++ coffee_news.js
@@ -13,7 +13,7 @@ $.get('https://newsapi.org/v1/articles?source=newsweek&sortBy=top&apiKey=d3c7138
 });
 
 $.get('https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=d3c7138c3b3b4fbfa75ea4c537b2a552', function (data) {
-  console.log(data.articles);
+  // console.log(data.articles);
   var unorderedList = document.createElement('ul');
   for (var i = 0; i < data.articles.length; i++) {
     var list = document.createElement('li');
@@ -26,9 +26,10 @@ $.get('https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=d3c7138
   }
 });
 
+var horoscope = document.getElementById('sign').value;
 $.ajax({
     type: 'POST',
-    url: 'https://aztro.herokuapp.com?sign=aries&day=today',
+    url: 'https://aztro.herokuapp.com?sign=' + horoscope + '&day=today',
     success: function (data) {
       console.log(data);
     },
